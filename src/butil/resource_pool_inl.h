f21afade47773b35064fb9f9004d7c3910920603
zhanfujie <zhanfujie@bigo.sg>
2019-11-04 21:15:04 +0800

    try fix7

--- src/butil/resource_pool_inl.h
+++ src/butil/resource_pool_inl.h
@@ -96,7 +96,7 @@ public:
 
 
 template <typename T>
-class alignas(128) ResourcePool {
+class ResourcePool {
 public:
     static const size_t BLOCK_NITEM = ResourcePoolBlockItemNum<T>::value;
     static const size_t FREE_CHUNK_NITEM = BLOCK_NITEM;
